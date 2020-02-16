481463343ec5da5cd32db3cedd13880fd618edc0
Yolanda3104 <54901735+Yolanda3104@users.noreply.github.com>
2020-02-16 18:37:12 +0800

    Update MainClass.java

--- SRP/MainClass.java
+++ SRP/MainClass.java
@@ -10,7 +10,7 @@ public class MainClass {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-              new MainClass();//³ÌĞòÈë¿Ú
+              new MainClass();
     }
 
 }
