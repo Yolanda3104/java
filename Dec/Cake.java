9cb2ff786781dedad60ca99baf4a28e8b8e059af
Yolanda3104 <54901735+Yolanda3104@users.noreply.github.com>
2020-04-30 12:55:47 +0800

    Add files via upload

---
+++ Dec/Cake.java
@@ -0,0 +1,14 @@
+package Dec;
+
+public class Cake implements IBirthdayCake {
+	public Cake() {
+		System.out.println("Cake blank was Created");
+	}
+
+	@Override
+	public void Show() {
+		// TODO Auto-generated method stub
+		System.out.println("Cake Blank");
+	}
+
+}
---
+++ Dec/Decorating.java
@@ -0,0 +1,16 @@
+package Dec;
+
+public class Decorating implements IBirthdayCake {
+	private IBirthdayCake birthdayCake;
+	
+	public Decorating(IBirthdayCake birthdayCake) {
+		this.birthdayCake = birthdayCake;
+	}
+	
+	@Override
+	public void Show() {
+		// TODO Auto-generated method stub
+		birthdayCake.Show();
+	}
+
+}
